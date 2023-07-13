9fb85fb2f9b6bed0b12a02e7fb8e3580fad3be3d
Ajay Kumar <ajaykumar@spicegems.com>
2023-07-13 12:26:43 +0530

    chap 2 complete-use routes

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+// chap 1
+
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port http://localhost:${port}`)
+// })
+
+
+// chap 2
+
 const express = require('express')
 const app = express()
 const port = 3000
+const path=require("path")
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.use("/",require(path.join(__dirname,"routes/blog.js")))
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
