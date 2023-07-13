fc8a41641d8a7421bf96d9a34ed825c70d2473f6
Ajay Kumar <ajaykumar@spicegems.com>
2023-07-13 16:23:51 +0530

    chap 3 complete

--- index.js
+++ index.js
@@ -15,6 +15,24 @@
 
 // chap 2
 
+// const express = require('express')
+// const app = express()
+// const port = 3000
+// const path=require("path")
+
+// // app.get('/', (req, res) => {
+// //   res.send('Hello World!')
+// // })
+
+// app.use("/",require(path.join(__dirname,"routes/blog.js")))
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port http://localhost:${port}`)
+// })
+
+// chap 3
+
+
 const express = require('express')
 const app = express()
 const port = 3000
--- routes/blog.js
+++ routes/blog.js
@@ -1,10 +1,40 @@
+// chap 2
+
+// const express =require("express")
+// const route=express.Router()
+
+
+// route.get("/",(req,res)=>{
+//   res.send("now page serve form blog.js")
+// })
+
+
+
+// module.exports=route
+
+
+// chap 3
+
 const express =require("express")
 const route=express.Router()
+const path=require("path")
 
 
 route.get("/",(req,res)=>{
-  res.send("now page serve form blog.js")
+  // res.send("now page serve form blog.js")
+  res.sendFile(path.join(__dirname,"../templates/index.html"))
+})
+route.get("/blog",(req,res)=>{
+  // res.send("now page serve form blog.js")
+  res.sendFile(path.join(__dirname,"../templates/blog.html"))
 })
 
+route.get("/blog/:slug",(req,res)=>{
+  // res.send("now page serve form blog.js")
+
+  // url k params ko get krne k liye use this: 
+  console.log(req.params.slug)
+  res.sendFile(path.join(__dirname,"../templates/blogpost.html"))
+})
 
 module.exports=route
